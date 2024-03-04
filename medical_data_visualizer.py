2b1ff0594813de9c474bdb146c9b08969296d019
Deninjo <imwendwadennis@gmail.com>
2024-03-04 21:31:48 +0000

    Normalised and added overweight column

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -11,6 +11,10 @@ df['BMI'] = (df['weight']/((df['height']/100)**2)).round(1)
 df['overweight'] = (df['BMI'] > 25).astype(int) # This converts the boolean values to integers 1 for True and vice versa
 
 # Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
+# Replace 0 with 'good' and 1 with 'bad'
+df[['active','smoke','alco','cardio','overweight']] = df[['active','smoke','alco','cardio','overweight']].replace({0: 'good', 1: 'bad'})
+# If value of cholesterol/gluc=1, make the value 0. If value > 1, make the value 1.
+df[['cholesterol', 'gluc']] = df[['cholesterol', 'gluc']].applymap(lambda x: 1 if x > 1 else 0)
 
 
 # Draw Categorical Plot
